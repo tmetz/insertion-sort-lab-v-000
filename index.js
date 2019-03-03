c384b6ca23d35b61af2d9473edaaf22513481ccc
Tammy Metz <tammy@tammymetz.com>
2019-03-03 03:48:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemove(array){
-
+  min = array[0];
+  minIndex = 0;
+  for (int i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
